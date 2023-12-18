b62d4a5fd9bbb635187ecbec801d884d26cf011f
Patarion <62268532+Patarion@users.noreply.github.com>
2023-12-18 10:57:00 -0500

    Add files via upload

--- Inception/src/requirements/MariaDB/tools/install_db.sh
+++ Inception/src/requirements/MariaDB/tools/install_db.sh
@@ -1,23 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "MariaDB : Configuring MariaDB...";
-if [ ! -d "/run/mysqld" ]; then
-    echo "-> (MariaDB) Granting MariaDB daemon run permissions...";
-    mkdir -p /run/mysqld;
-    chown -R mysql:mysql /run/mysqld;
+# Function to execute MySQL queries
+execute_mysql_query() {
+    mysql -uroot -p"$MARIADB_ROOT_PASSWORD" -e "$1"
+}
+
+# Check if MySQL is installed and start the service
+mysql_install_db
+service mysql start
+
+# Check if the database exists
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo "Database already exists"
+else
+    # Set root password non-interactively
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password $MARIADB_ROOT_PASSWORD"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MARIADB_ROOT_PASSWORD"
+	apt-get update
+
+    # Add a root user on 127.0.0.1 to allow remote connection
+    execute_mysql_query "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+
+    # Create a database and user in the database for WordPress
+    execute_mysql_query "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+    execute_mysql_query "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+
+    # Import the database in the MySQL command line
+    mysql -uroot -p"$MARIADB_ROOT_PASSWORD" $MYSQL_DATABASE < /usr/local/bin/mysql_setup.sql
 fi
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "MariaDB : Installing MySQL Data Directory...";
-    chown -R mysql:mysql /var/lib/mysql;
+# Stop the MySQL service
+service mysql stop
 
-    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=$MYSQL_USER;
-    sed -i "s/\r//g ; s/MYSQL_DATABASE/$MYSQL_DATABASE/g ; s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g ; s/MYSQL_USER/$MYSQL_USER/g ; s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /usr/local/mysql/mysql_setup.sql;
-
-    /usr/bin/mysqld --bootstrap < /usr/local/mysql/mysql_setup.sql;
-    echo "MariaDB : MySQL configuration done.";
-fi
-echo "MariaDB : Allowing remote connections to MariaDB";
-echo "MariaDB : listening on port 3306.";
-
+# Execute the provided command
 exec "$@"
